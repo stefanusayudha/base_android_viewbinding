0ca70a961f96956323e110b33f1547fbb313653a
stefanus ayudha <stefanus.ayudha@gmail.com>
2022-05-16 12:44:47 +0700

    add date util formatter

--- core/common/src/main/java/com/iddevops/core/common/util/Date.kt
+++ core/common/src/main/java/com/iddevops/core/common/util/Date.kt
@@ -1,5 +1,21 @@
 package com.iddevops.core.common.util
 
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.TimeZone
+
 fun timeToMillis(hour: Int = 0, minute: Int = 0, sec: Int = 0): Long {
     return ((hour * 60 * 60) + (minute * 60) + sec) * 1000L
+}
+
+// example : longMillisToFormattedTimeDate( 10000, "dd/MM/yyyy hh:mm a", "GMT+2" )
+fun longMillisToFormattedTimeDate(
+    unixTimeMillis: Long,
+    outputPattern: String,
+    timeZone: String? = null
+): String {
+    val formatter = SimpleDateFormat(outputPattern)
+    formatter.timeZone =
+        if (timeZone.isNullOrBlank()) TimeZone.getDefault() else TimeZone.getTimeZone(timeZone)
+    return formatter.format(Date(unixTimeMillis))
 }
