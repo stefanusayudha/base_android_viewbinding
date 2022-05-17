aa90806ec47f5d09bea1b98ec3da3c88bf1fd1c7
stefanus ayudha <stefanus.ayudha@gmail.com>
2022-05-17 10:27:52 +0700

    adding render engine for each object in view model implement exclusive presentation principle

--- modules/common/src/main/java/com/iddevops/common/presentation/fragment/todolist/TodoListFragment.kt
+++ modules/common/src/main/java/com/iddevops/common/presentation/fragment/todolist/TodoListFragment.kt
@@ -8,9 +8,11 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.lifecycle.lifecycleScope
 import com.iddevops.common.databinding.FragmentTodoListBinding
+import com.iddevops.common.domain.model.TodoModel
 import com.iddevops.common.presentation.adapter.TodoAdapter
 import com.iddevops.core.common.data.request.RequestState
 import com.iddevops.core.common.presentation.base.BaseFragment
+import kotlinx.coroutines.flow.FlowCollector
 import kotlinx.coroutines.launch
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -42,7 +44,7 @@ class TodoListFragment : BaseFragment<FragmentTodoListBinding>() {
         todoAdapter = TodoAdapter(
             requireContext(),
             onRequestLoadMore = {
-                with(vm){
+                with(vm) {
                     getMoreTodos()
                 }
             }
@@ -55,74 +57,72 @@ class TodoListFragment : BaseFragment<FragmentTodoListBinding>() {
     }
 
     override fun initAction() {
-        with(Handler(Looper.getMainLooper())) {
-            postDelayed({ todoAdapter?.setSpanCount(1) }, 7000)
-            postDelayed({ todoAdapter?.setSpanCount(2) }, 9000)
-            postDelayed({ todoAdapter?.setSpanCount(3) }, 11000)
-        }
+        // uncomment bellow to preview span count modifier
+//        with(Handler(Looper.getMainLooper())) {
+//            postDelayed({ todoAdapter?.setSpanCount(1) }, 7000)
+//            postDelayed({ todoAdapter?.setSpanCount(2) }, 9000)
+//            postDelayed({ todoAdapter?.setSpanCount(3) }, 11000)
+//        }
     }
 
     override fun initObserver() {
         with(lifecycleScope) {
-            launch {
-                vm.cacheTodos.collect {
-                    when (it) {
-                        is RequestState.Default -> {}
-                        is RequestState.Loading -> {
-                            with(todoAdapter){
-                                this?.showLoadMore()
-                            }
-                        }
-                        is RequestState.Success -> {
-                            if (it.data.isEmpty())
-                                with(vm){
-                                    getTodos()
-                                }
-                            else
-                                with(todoAdapter){
-                                    this?.submitList(it.data)
-                                }
-                        }
-                        is RequestState.Failed -> {
-                            with(vm){
-                                getTodos()
-                            }
-                        }
-                        else -> {
-
-                        }
-                    }
-                }
-            }
-
-            launch {
-                vm.listTodos.collect {
-                    when (it) {
-                        is RequestState.Default -> {}
-                        is RequestState.Failed -> {
-                            Toast.makeText(
-                                this@TodoListFragment.requireContext(),
-                                "Failed to load list",
-                                Toast.LENGTH_SHORT
-                            ).show()
-                        }
-                        is RequestState.Loading -> {
-                            with(todoAdapter){
-                                this?.showLoadMore()
-                            }
-                        }
-                        is RequestState.Success -> {
-                            with(todoAdapter){
-                                this?.submitList(it.data)
-                            }
-                        }
-                    }
-                }
-            }
-
+            launch { vm.cacheTodos.collect(cacheTodosRenderEngine) }
+            launch { vm.listTodos.collect(todoListRenderEngine) }
             launch {
                 vm.canLoadMore.collect { canLoadMore ->
                     todoAdapter?.canLoadMore(canLoadMore)
+                }
+            }
+        }
+    }
+
+    private val cacheTodosRenderEngine = FlowCollector<RequestState<List<TodoModel>>> {
+        when (it) {
+            is RequestState.Default -> {}
+            is RequestState.Loading -> {
+                with(todoAdapter) {
+                    this?.showLoadMore()
+                }
+            }
+            is RequestState.Success -> {
+                if (it.data.isEmpty())
+                    with(vm) {
+                        getTodos()
+                    }
+                else
+                    with(todoAdapter) {
+                        this?.submitList(it.data)
+                    }
+            }
+            is RequestState.Failed -> {
+                with(vm) {
+                    getTodos()
+                }
+            }
+            else -> {
+
+            }
+        }
+    }
+    private val todoListRenderEngine = FlowCollector<RequestState<List<TodoModel>>> {
+        when (it) {
+            is RequestState.Default -> {}
+            is RequestState.Failed -> {
+                Toast.makeText(
+                    this@TodoListFragment.requireContext(),
+                    "Failed to load list",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+            is RequestState.Loading -> {
+                with(todoAdapter) {
+                    this?.showLoadMore()
+                }
+            }
+            is RequestState.Success -> {
+                with(todoAdapter) {
+                    this?.submitList(it.data)
                 }
             }
         }
