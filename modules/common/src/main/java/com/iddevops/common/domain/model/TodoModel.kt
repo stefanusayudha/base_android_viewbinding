c9c531d7c05a019f322c0e8c475f83a148de3176
stefanus ayudha <stefanus.ayudha@gmail.com>
2022-06-09 18:58:48 +0700

    domain model non nullable

--- modules/common/src/main/java/com/iddevops/common/domain/model/TodoModel.kt
+++ modules/common/src/main/java/com/iddevops/common/domain/model/TodoModel.kt
@@ -3,8 +3,8 @@ package com.iddevops.common.domain.model
 import com.iddevops.core.common.util.ID
 
 data class TodoModel(
-    val id: ID?,
-    val completed: Boolean?,
-    val title: String?,
-    val userId: ID?
+    val id: ID,
+    val completed: Boolean,
+    val title: String,
+    val userId: ID
 )
